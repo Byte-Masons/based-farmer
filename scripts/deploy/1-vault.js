506ccc1f43e1372df16894b0b1b995110f62df04
Magnus Brantheim <m.brantheim@gmail.com>
2022-03-15 18:24:27 +0100

    Redeployed with poolId set in initialize

--- scripts/deploy/1-vault.js
+++ scripts/deploy/1-vault.js
@@ -1,12 +1,12 @@
 async function main() {
   const Vault = await ethers.getContractFactory('ReaperVaultv1_3');
 
-  const wantAddress = '0xCa395560B6003D921D9408aF011C6C61399F66cA';
-  const tokenName = 'SPIRIT-RAINSPIRIT Solidex Crypt';
-  const tokenSymbol = 'rfsAMM-SPIRIT-RAINSPIRIT';
+  const wantAddress = '0x6F607443DC307DCBe570D0ecFf79d65838630B56';
+  const tokenName = 'FTM-BSHARE Based Crypt';
+  const tokenSymbol = 'rf-BASED-FTM-BSHARE';
   const depositFee = 0;
   const tvlCap = ethers.utils.parseEther('2000');
-  const options = { gasPrice: 300000000000, gasLimit: 9000000 };
+  const options = { gasPrice: 600000000000, gasLimit: 9000000 };
 
   const vault = await Vault.deploy(wantAddress, tokenName, tokenSymbol, depositFee, tvlCap, options);
 
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,11 +1,11 @@
 async function main() {
-  const vaultAddress = '0x2FeF389532D7Efef5402C3CEB77b88E082F3872b';
-  const strategyAddress = '0xc515392C35270A5d7bD9eeE15b55eD8a4b9F79E0';
+  const vaultAddress = '0x258D9405D222a5aE17011AE74A092c239018B305';
+  const strategyAddress = '0x666bE8B3cAD9316490c3a57514f616f0B1BBdBD6';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_3');
   const vault = Vault.attach(vaultAddress);
 
-  const options = { gasPrice: 1000000000000, gasLimit: 9000000 };
+  const options = { gasPrice: 500000000000, gasLimit: 9000000 };
   await vault.initialize(strategyAddress, options);
   console.log('Vault initialized');
 }
