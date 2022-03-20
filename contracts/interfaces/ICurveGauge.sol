c427f2ac3aab0b9c62b62586ac88c4a841a7ba65
Magnus Brantheim <m.brantheim@gmail.com>
2022-03-20 18:28:32 +0100

    Modified strategy for curve tricrypto

---
+++ contracts/interfaces/ICurveGauge.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.11;
+
+interface ICurveGauge {
+    function deposit(uint256 _value) external;
+}
---
+++ contracts/interfaces/ICurveLiquidityPool.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.11;
+
+interface ICurveLiquidityPool {
+    function add_liquidity(uint256[3] memory _amounts, uint256 _min_mint_amount) external;
+
+    function token() external returns (address);
+}
