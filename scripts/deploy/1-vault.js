9dd7509757fa9294873ed6f0f99e445f4fd058a7
Bastien Guyonnet <bastien.guyonnet@tutanota.com>
2022-06-10 21:50:52 +0200

    deployed

--- scripts/deploy/1-vault.js
+++ scripts/deploy/1-vault.js
@@ -1,12 +1,12 @@
 async function main() {
   const Vault = await ethers.getContractFactory('ReaperVaultv1_3');
 
-  const wantAddress = '0xCa395560B6003D921D9408aF011C6C61399F66cA';
-  const tokenName = 'SPIRIT-RAINSPIRIT Solidex Crypt';
-  const tokenSymbol = 'rfsAMM-SPIRIT-RAINSPIRIT';
+  const wantAddress = '0x58e57cA18B7A47112b877E31929798Cd3D703b0f';
+  const tokenName = 'Tricrypto Based V2 Crypt';
+  const tokenSymbol = 'rf-based-crv3crypto';
   const depositFee = 0;
   const tvlCap = ethers.utils.parseEther('2000');
-  const options = { gasPrice: 300000000000, gasLimit: 9000000 };
+  const options = { gasPrice: 400000000000, gasLimit: 9000000 };
 
   const vault = await Vault.deploy(wantAddress, tokenName, tokenSymbol, depositFee, tvlCap, options);
 
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = '0x2FeF389532D7Efef5402C3CEB77b88E082F3872b';
-  const strategyAddress = '0xc515392C35270A5d7bD9eeE15b55eD8a4b9F79E0';
+  const vaultAddress = '0x73eA400B8188e59C929d1987e7DDF20691d24D86';
+  const strategyAddress = '0x8C8AD36963f2fFA16578141083e949D2d1B20B27';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_3');
   const vault = Vault.attach(vaultAddress);
